322f1af4cab2c52314602130277b327a82650ddb
grizz479 <diego.aylagas@gmail.com>
2021-07-18 15:40:59 +0200

    GET completo hasta vista por post

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'clave_secreta'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
     
